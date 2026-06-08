2fe9b97de29cadeea4742a9d05d8868b1cfc1d6c
Aridcf05 <89971210+Aridcf05@users.noreply.github.com>
2026-06-08 13:10:25 -0500

    Add files via upload

---
+++ SwiftUITraining/SwiftUI Ejercicios/SwiftUI_EjerciciosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EjerciciosApp.swift
+//  SwiftUI Ejercicios
+//
+//  Created by Ariana Ileen Del Carpio Flores on 4/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EjerciciosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PersonalCardView()
+        }
+    }
+}
