443524a0e155646c348de6a40783ff7bcdf17a3a
Anani <89971210+Aridcf05@users.noreply.github.com>
2026-06-15 20:18:45 -0500

    Add files via upload

---
+++ prueba/prueba/pruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaApp.swift
+//  prueba
+//
+//  Created by Ariana Ileen Del Carpio Flores on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct pruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
